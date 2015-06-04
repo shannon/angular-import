6eab07ff198771121ea1b4d5bf051bc800826f86
Shannon Poole <shannon.m.poole@gmail.com>
2015-06-04 17:32:08 -0400

    support for vulcanize

--- angular-import.js
+++ angular-import.js
@@ -2,20 +2,20 @@ angular.module('templateImport', [])
   .run(function($templateCache){
     var links = [].slice.call(document.querySelectorAll('link[rel=import]'));
     var link, template;
+    var templates = [].slice.call(document.querySelectorAll('script[type="text/ng-template"], template'));
     
     while(links.length){
       link = links.shift();
       
       if(link.import){
-        var templates = [].slice.call(link.import.querySelectorAll('script[type="text/ng-template"], template'));
-        
-        while(templates.length){
-          template = templates.shift();
-          $templateCache.put(template.id, template.innerHTML.trim());
-        }
-        
+        templates = templates.concat([].slice.call(link.import.querySelectorAll('script[type="text/ng-template"], template')));
         links.push.apply(links, link.import.querySelectorAll('link[rel=import]'));
       }
+    }
+  
+    while(templates.length){
+      template = templates.shift();
+      $templateCache.put(template.id, template.innerHTML.trim());
     }
   })
 ;
